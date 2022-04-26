3bd58e89b4d5b3eb5b357ed8918ea412a8f51a4d
RainergrenOscar <oscar.rainergren@hotmail.com>
2022-04-26 09:52:02 +0200

    added front end😜

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss"
+
+function App() {
+	return (
+		<div className='App'>
+			<div className='cha'>
+				<h1>HELLO</h1>
+			</div>
+			<h1>HELLO</h1>
+		</div>
+	)
+}
+
+export default App
